be474b1d8141b22dcd026cfec4b19a804a6f1c34
박동현 <jinee9523@gmail.com>
2025-07-21 14:40:57 +0900

    Test: Username Unit Test

---
+++ domains/user/domain/username_test.go
@@ -0,0 +1,75 @@
+package domain
+
+import (
+	"github.com/pdh9523/go-hexarch/domains/user/domain/errorCode"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestNewUsername(t *testing.T) {
+	t.Run("Given valid username when create username then should return user", func(t *testing.T) {
+		testUsername := "testusername"
+		username, err := NewUsername(testUsername)
+
+		assert.NoError(t, err)
+		assert.Equal(t, testUsername, username.ToString())
+	})
+
+	t.Run("Given short username when create username then should return error", func(t *testing.T) {
+		testUsername := "1"
+		username, err := NewUsername(testUsername)
+		assert.Nil(t, username)
+		assert.ErrorIs(t, err, errorCode.ErrUsernameTooShort)
+	})
+
+	t.Run("Given long username when create username then should return error", func(t *testing.T) {
+		testUsername := "1234567890123456789012345678901234576890"
+		username, err := NewUsername(testUsername)
+		assert.Nil(t, username)
+		assert.ErrorIs(t, err, errorCode.ErrUsernameTooLong)
+	})
+
+	t.Run("Given username when compare same username should return true", func(t *testing.T) {
+		testUsername := "testusername"
+		username1, err := NewUsername(testUsername)
+		assert.NoError(t, err)
+		username2, err := NewUsername(testUsername)
+		assert.NoError(t, err)
+		assert.Equal(t, true, username1.Equals(username2))
+	})
+
+	t.Run("Given username with no length should return false", func(t *testing.T) {
+		testUsername := ""
+		username, err := NewUsername(testUsername)
+		assert.Nil(t, username)
+		assert.ErrorIs(t, err, errorCode.ErrUsernameTooShort)
+	})
+
+	t.Run("Given username with invalid Character should return false", func(t *testing.T) {
+		testUsername := "%test%"
+		username, err := NewUsername(testUsername)
+		assert.Nil(t, username)
+		assert.ErrorIs(t, err, errorCode.ErrUsernameInvalidCharacters)
+	})
+
+	t.Run("Given username with blank character should return false", func(t *testing.T) {
+		testUsername := "  "
+		username, err := NewUsername(testUsername)
+		assert.Nil(t, username)
+		assert.ErrorIs(t, err, errorCode.ErrUsernameInvalidCharacters)
+	})
+
+	t.Run("Given username with Korean should return false", func(t *testing.T) {
+		testUsername := "테스트유저"
+		username, err := NewUsername(testUsername)
+		assert.Nil(t, username)
+		assert.ErrorIs(t, err, errorCode.ErrUsernameInvalidCharacters)
+	})
+
+	t.Run("Given upper case character should return false", func(t *testing.T) {
+		testUsername := "TESTUSER"
+		username, err := NewUsername(testUsername)
+		assert.Nil(t, username)
+		assert.ErrorIs(t, err, errorCode.ErrUsernameInvalidCharacters)
+	})
+}
