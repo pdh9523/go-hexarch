e32ff120974b86a964cc78306f7e15732a4111a0
박동현 <jinee9523@gmail.com>
2025-07-23 01:58:25 +0900

    Feat: Add DeleteByID Method

--- domains/user/application/port/out/user_repository_port.go
+++ domains/user/application/port/out/user_repository_port.go
@@ -8,9 +8,9 @@ import (
 type UserRepositoryPort interface {
 	FindByID(ctx context.Context, id string) (*domain.User, error)
 	FindByNickname(ctx context.Context, nickname string) (*domain.User, error)
-	ExistsByID(ctx context.Context, id string) (bool, error)
 	ExistsByNickname(ctx context.Context, nickname string) (bool, error)
 	ExistsByUsername(ctx context.Context, username string) (bool, error)
 	Save(ctx context.Context, user *domain.User) (*domain.User, error)
 	Update(ctx context.Context, user *domain.User) error
+	DeleteByID(ctx context.Context, id string) error
 }
