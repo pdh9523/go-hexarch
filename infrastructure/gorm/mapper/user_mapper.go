55ec52538bd9e345ca5447fa4524e78ae66c3e80
박동현 <jinee9523@gmail.com>
2025-07-23 02:01:47 +0900

    Feat: User Mapper

---
+++ infrastructure/gorm/mapper/user_mapper.go
@@ -0,0 +1,90 @@
+package mapper
+
+import (
+	"errors"
+	"github.com/pdh9523/go-hexarch/domains/user/domain"
+	userError "github.com/pdh9523/go-hexarch/domains/user/domain/errorCode"
+	"github.com/pdh9523/go-hexarch/infrastructure/gorm/model"
+	commonError "github.com/pdh9523/go-hexarch/shared/common/errorCode"
+	"gorm.io/gorm"
+)
+
+func ToDomainUser(userModel *model.User) (*domain.User, error) {
+	role := stringToRole(userModel.Role)
+	username, err := domain.NewUsername(userModel.Username)
+	if err != nil {
+		return nil, err
+	}
+	nickname, err := domain.NewNickname(userModel.Nickname)
+	if err != nil {
+		return nil, err
+	}
+
+	return &domain.User{
+		ID:       userModel.ID,
+		Username: username,
+		Nickname: nickname,
+		Role:     role,
+	}, nil
+}
+
+func ToModelUser(userDomain *domain.User) *model.User {
+	role := string(userDomain.Role)
+	username := userDomain.Username.ToString()
+	nickname := userDomain.Nickname.ToString()
+
+	user := &model.User{
+		Username: username,
+		Nickname: nickname,
+		Role:     role,
+	}
+
+	if userDomain.ID != "" {
+		user.ID = userDomain.ID
+	}
+
+	return user
+}
+
+func stringToRole(role string) domain.Role {
+	var result domain.Role
+	switch role {
+	case "ADMIN":
+		result = domain.ROLE_ADMIN
+	case "USER":
+		result = domain.ROLE_USER
+	default:
+		result = domain.ROLE_USER
+	}
+	return result
+}
+
+func ToDomainError(err error) error {
+	if err == nil {
+		return nil
+	}
+
+	switch {
+	case errors.Is(err, gorm.ErrRecordNotFound):
+		return userError.ErrUserNotFound
+	case errors.Is(err, gorm.ErrDuplicatedKey):
+		return userError.ErrUserAlreadyExists
+	case errors.Is(err, gorm.ErrInvalidTransaction):
+		return commonError.ErrInternalServerError
+	case errors.Is(err, gorm.ErrNotImplemented):
+		return commonError.ErrInternalServerError
+	case errors.Is(err, gorm.ErrMissingWhereClause):
+		return commonError.ErrInvalidRequest
+	case errors.Is(err, gorm.ErrUnsupportedRelation):
+		return commonError.ErrInternalServerError
+	case errors.Is(err, gorm.ErrPrimaryKeyRequired):
+		return commonError.ErrInvalidRequest
+	case errors.Is(err, gorm.ErrModelValueRequired):
+		return commonError.ErrInvalidRequest
+	case errors.Is(err, gorm.ErrInvalidData):
+		return commonError.ErrInvalidRequest
+	default:
+		// 알 수 없는 DB 에러는 내부 서버 에러로 처리
+		return commonError.ErrInternalServerError
+	}
+}
