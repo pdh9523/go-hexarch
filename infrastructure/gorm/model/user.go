8aca3869af8c94f853a41d88cead8f874b939973
박동현 <jinee9523@gmail.com>
2025-07-23 13:39:07 +0900

    Fix: Nickname should be unique

--- infrastructure/gorm/model/user.go
+++ infrastructure/gorm/model/user.go
@@ -3,6 +3,6 @@ package model
 type User struct {
 	BaseModel
 	Username string `gorm:"unique;size:20;not null"`
-	Nickname string `gorm:"size:20;not null"`
+	Nickname string `gorm:"unique;size:20;not null"`
 	Role     string `gorm:"size:16;not null"`
 }
