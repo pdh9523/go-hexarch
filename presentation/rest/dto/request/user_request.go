0cac3e5111a393415b063e04e146be79161097a9
박동현 <jinee9523@gmail.com>
2025-08-10 00:25:47 +0900

    Fix: form tags to json for POST methods

--- presentation/rest/dto/request/user_request.go
+++ presentation/rest/dto/request/user_request.go
@@ -9,23 +9,23 @@ type CheckUsernameAvailabilityRequest struct {
 }
 
 type SignUpRequest struct {
-	Username string `form:"username" binding:"required,min=2,max=16"`
-	Password string `form:"password" binding:"required,min=8,max=32"`
-	Nickname string `form:"nickname" binding:"required,min=2,max=20"`
+	Username string `json:"username" binding:"required,min=2,max=16"`
+	Password string `json:"password" binding:"required,min=8,max=32"`
+	Nickname string `json:"nickname" binding:"required,min=2,max=20"`
 }
 
 type SignInRequest struct {
-	Username string `form:"username" binding:"required,min=2,max=16"`
-	Password string `form:"password" binding:"required,min=8,max=32"`
+	Username string `json:"username" binding:"required,min=2,max=16"`
+	Password string `json:"password" binding:"required,min=8,max=32"`
 }
 
 type ChangePasswordRequest struct {
-	CurrentPassword string `form:"current_password" binding:"required,min=8,max=32"`
-	NewPassword     string `form:"new_password" binding:"required,min=8,max=32"`
+	CurrentPassword string `json:"current_password" binding:"required,min=8,max=32"`
+	NewPassword     string `json:"new_password" binding:"required,min=8,max=32"`
 }
 
 type ChangeNicknameRequest struct {
-	Nickname string `form:"nickname" binding:"required,min=2,max=20"`
+	Nickname string `json:"nickname" binding:"required,min=2,max=20"`
 }
 
 type RefreshTokenRequest struct {
